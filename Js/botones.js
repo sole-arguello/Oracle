1d890c3abcb5a4ebe097f7a36a1cb982e9e7bdb0
Soledad <soledad.ar1@mail.com>
2022-01-24 21:12:14 -0300

    codigo funcionando, falta hacer funcionar boton pegar

--- Js/botones.js
+++ Js/botones.js
@@ -1,44 +1,57 @@
 //declaro variables que del Input y Output
 var textInput = document.querySelector("#textInput");//texto que ingresa para encriptar y desencriptar
 var textOutput = document.querySelector("#textOutput");//el mensaje que recibe para ser copiado(msg)
+var errorTexto = document.querySelector("#error-texto");
 
-//capturo el boton encriptar
-var btnEncriptar = document.querySelector("#btn-encriptar");
-//escucho el boton encriptar
-btnEncriptar.addEventListener("click", function(event){
-    event.preventDefault();
-    //recibe el texto a encriptar
-    //console.log(textInput.value);
-    validacionCaracteres();
-    textOutput.value = encriptar(textInput.value);
-    textInput.value = "";
-});
-
-//capturo el boton desencriptar
-var btnDesencriptar = document.querySelector("#btn-desencriptar");
-//escucho el boton desenciptar
-btnDesencriptar.addEventListener("click", function(event){
-    event.preventDefault();
-    //recibe el texto a desencriptar
-    //console.log(textInput.value);
-    validacionCaracteres();
-    textOutput.value = Desencriptar(textInput.value);
-    //vacia el input de entrada
-    textInput.value = "";
-
+textInput.addEventListener("input", function(){
+    var expresiones = /[A-Zéáíóú]/;
+    
+    if(textInput.value.length > 0 && expresiones.test(textInput.value)){
+        errorTexto.classList.remove("invisible");
+        swal({
+            title:"Error!!",
+            text: "Carracter Invalido",
+            icon: "error",
+            buttons :false,
+            timer:3000
+        });        
+    }else{
+        errorTexto.classList.add("invisible");
+        //capturo el boton encriptar
+        var btnEncriptar = document.querySelector("#btn-encriptar");
+        //escucho el boton encriptar
+        btnEncriptar.addEventListener("click", function(event){
+            event.preventDefault();
+            //recibe el texto a encriptar
+            textOutput.value = encriptar(textInput.value);
+        });
+        //capturo el boton desencriptar
+        var btnDesencriptar = document.querySelector("#btn-desencriptar");
+        //lo escucho
+        btnDesencriptar.addEventListener("click", function(event){
+            event.preventDefault();
+            //recibe el texto a desencriptar
+            textOutput.value = Desencriptar(textInput.value);
+        });
+    }
 });
 
 //capturo el boton copiar
 var btnCopiar = document.querySelector("#btn-copy");
-// escucho
+// lo escucho
 btnCopiar.addEventListener("click", function(){
+    //alerta copiado
+    swal({
+        title:"Texto Copiado!!",
+        text : "LO PEGA EN ENTRADA",
+        icon: "success",
+        buttons :false,
+        timer:3000
+    });
     //toma el mensaje del textOutput
     navigator.clipboard.writeText(textOutput.value);
-    //vacia el campo mensaje luego de copiar
+    //vacia salida del mensaje luego de copiar
     textOutput.value = "";
-    
-    console.log();
-
 });
 /*
 var btnPegar = document.querySelector("#btn-pegar");
--- Js/funciones.js
+++ Js/funciones.js
@@ -1,15 +1,26 @@
 //mensaje --> es el input ya sea para encriptar o desencriptar
 function encriptar(mensaje){
-    
+    swal({
+        text:"Mensaje Encriptado",
+        icon: "success",
+        buttons :false,
+        timer:3000
+    });    
     var mensajeOutput = mensaje.replaceAll("e", "enter")
                                .replaceAll("i", "imes")
                                .replaceAll("a", "ai")
                                .replaceAll("o", "ober")
                                .replaceAll("u", "ufat");
     return mensajeOutput;
-
+    
 }
 function Desencriptar(mensaje){
+    swal({
+        text:"Mensaje Desencriptado",
+        icon: "success",
+        buttons :false,
+        timer:3000
+    });
     var mensajeOutput = mensaje.replaceAll("enter", "e")
                                .replaceAll("imes", "i")
                                .replaceAll("ai", "a")
@@ -18,55 +29,10 @@ function Desencriptar(mensaje){
     return mensajeOutput;
 }
 
-function aletaError(){
-    swal({
-        title:"Error!!",
-        text: "Caracter Incorrecto",
-        icon: "error",
-        buttons :false,
-        timer:2000
-        
-    });
-}
 //----------------------------------------------------------------------------------
-function validacionCaracteres(textoInput){
-
-    var reglaValidacion = new RegExp("^[A-Zéáíóú]", "g");
-    var ingresado = textoInput.value;
-
-    if(ingresado == ""){
-
-        alert("encriptar");
-        return false;
-    }else if(!reglaValidacion.test(ingresado)){
-        return true;
-    }else{
-        aletaError();
-        //return false;
-    }
-}
 
 
-/*
-function validacionCaracteres(){
-    var campoForm = document.querySelector("#textInput");
-    console.log(campoForm.value);
-    campoForm.addEventListener("input", function(){
-        var expresiones= /^[A-Zéáíóú]/g;
-        console.log(expresiones)
-        var errorTexto = document.querySelector("#error-texto");
-        console.log(expresiones.test(campoForm.value))
-        if(campoForm.value.length > 0 && !expresiones.test(campoForm.value)){
-            errorTexto.classList.remove("invisible");
-            console.log("malo")
-            aletaError();
-        }else{
-            errorTexto.classList.add("invisible");
-            console.log("bueno")
-        }
-        
-    });
-}
+
 
    
 
